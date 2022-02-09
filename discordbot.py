51621f83b5632738545fd31d475df93080701899
SIGESPLIDE <98093993+SIGESPLIDE@users.noreply.github.com>
2022-02-09 20:12:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,11 +16,26 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
-    
 @bot.command()
 async def yattaze(ctx):
     await ctx.send('やったぜ')
         
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
 
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+@bot.cmmands()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")        
+        
+        
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
