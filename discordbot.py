20d5286b07d5f7cbab4c10eb615aee303acd955f
SIGESPLIDE <98093993+SIGESPLIDE@users.noreply.github.com>
2022-02-09 16:27:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,8 +15,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong'):
-async def maji(ctx)
-    await ctx.send('まじ？'):
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
