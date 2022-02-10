fe02d656a2d51407c0db9c4b707e298d9eeea9a2
SIGESPLIDE <98093993+SIGESPLIDE@users.noreply.github.com>
2022-02-10 22:38:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,8 +18,16 @@ async def on_command_error(ctx, error):
 
 @client.event
 async def on_ready():
-    print('起動しました.')
-    await client.change_presence(activity=discord.Game(name=f"{len(client.guilds)} 鯖で稼働中!"))
+
+# 認識しているサーバーをlist型で取得し、その要素の数を 変数:guild_count に格納
+    guild_count = len(client.guilds)
+# 関数:lenは、引数に指定したオブジェクトの長さや要素の数を取得
+    game = discord.Game(f'{guild_count} サーバー数に導入されています')
+# BOTのステータスを変更する
+    await client.change_presence(status=discord.Status.online, activity=game)
+# パラメーターの status でステータス状況(オンライン, 退席中など)を変更可能
+
+
 
 
 @bot.command()
