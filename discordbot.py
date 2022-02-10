28d9eb2b1416e4cc85d9727093eb115df8b2abe9
SIGESPLIDE <98093993+SIGESPLIDE@users.noreply.github.com>
2022-02-10 22:16:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -58,12 +58,8 @@ async def info(ctx):
 
 @bot.command()
 async def omikuji(ctx):
-
-#listを作る
     OmikujiList = ['大吉', '吉', '中吉', '小吉', '半吉', '末吉', '末小吉', '平', '凶', '小凶', '半凶', '末凶', '大凶']
-
-#listの中身をランダムに取得
-    print("あなたの運勢は" + random.choice(OmikujiList) + "です")
+    await ctx.send("あなたの運勢は" + random.choice(OmikujiList) + "です")
 
 
 token = getenv('DISCORD_BOT_TOKEN')
