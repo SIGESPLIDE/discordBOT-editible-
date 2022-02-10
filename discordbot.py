c76a61f14e2a833ddf983317b14971c1075b37b8
SIGESPLIDE <98093993+SIGESPLIDE@users.noreply.github.com>
2022-02-10 22:39:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,7 +22,7 @@ async def on_ready():
 # 認識しているサーバーをlist型で取得し、その要素の数を 変数:guild_count に格納
     guild_count = len(client.guilds)
 # 関数:lenは、引数に指定したオブジェクトの長さや要素の数を取得
-    game = discord.Game(f'{guild_count} サーバー数に導入されています')
+    game = discord.Game(f'{guild_count} 鯖で稼働中')
 # BOTのステータスを変更する
     await client.change_presence(status=discord.Status.online, activity=game)
 # パラメーターの status でステータス状況(オンライン, 退席中など)を変更可能
