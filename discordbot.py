6526b6ff1c3b189d64ea104d9fb36249b5fa0c87
SIGESPLIDE <98093993+SIGESPLIDE@users.noreply.github.com>
2022-02-11 21:46:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,6 +23,20 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    ch = int型のチャンネルID
+    embed = discord.Embed(title="エラー情報", description="", color=0xf00)
+    embed.add_field(name="エラー発生サーバー名", value=ctx.guild.name, inline=False)
+    embed.add_field(name="エラー発生サーバーID", value=ctx.guild.id, inline=False)
+    embed.add_field(name="エラー発生ユーザー名", value=ctx.author.name, inline=False)
+    embed.add_field(name="エラー発生ユーザーID", value=ctx.author.id, inline=False)
+    embed.add_field(name="エラー発生コマンド", value=ctx.message.content, inline=False)
+    embed.add_field(name="発生エラー", value=error, inline=False)
+    m = await bot.get_channel(ch).send(embed=embed)
+    await ctx.send(f"何らかのエラーが発生しました。\nこのエラーについて問い合わせるときはこのコードも一緒にお知らせください：{m.id}")
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
