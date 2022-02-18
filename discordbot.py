f21acb0d5d10bade6c52b8488b915f37d35267be
SIGESPLIDE <98093993+SIGESPLIDE@users.noreply.github.com>
2022-02-18 21:58:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,22 +1,75 @@
 # -*- coding: utf-8 -*-
 # ----------必要なパッケージを読み込み----------
 # 標準パッケージじゃない
+from ast import Index
+from webbrowser import get
 import timeout_decorator
 import discord
 import youtube_dl
+#from requests import get
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+from discord.utils import get
 
 # 標準パッケージ
 import datetime
 import random
 import re
-import time
 import os
 import asyncio
+
+
+#　YOUTUBE_DL用定義
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
 
 # ----------ボットの定義----------
 client = discord.Client()
 prefix = "\$"
+list_OKNO  = ['👍','👎']
 list_yesno = ['⭕', '❌']
 list_vote = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
 
@@ -50,11 +103,6 @@ async def on_message(message):
     JST        = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
     date       = datetime.datetime.now(JST)
     print(f"[{date.strftime('%Y年%m月%d日 %H:%M:%S')}] {message.guild.name} >> {message.channel.name} >> {message.author.name}:{message.content}")
-    #try:
-        # print(f"{message.guild.name}で{message.channel.name}に{message.author.name}がメッセージをおくったよん")
-        # await message.channel.send(f"{message.guild.name}で{message.channel.name}に{message.author.name}がメッセージをおくったよん")
-    #except Exception as e:
-    #     print(f"Error Send: {e.args}")
     # botチェック
     if message.author.bot:
         return
@@ -64,14 +112,35 @@ async def on_message(message):
         for i in range(len(list_vote)):
             await message.add_reaction(list_vote[i])
         return
+    # オウム返し
+    if isCommand(message,"rep(|eat)$") or isCommand(message,"rep(|eat)"):
+        await message.delete()
+        try:
+            repData  = message.content.split("\\")
+            repType  = repData[1]
+        except Exception as e:
+            print(e.args)
+        try:
+            await message.channel.send(repType)
+        except IndexError:
+            await message.channel.send("文字が入力されていません。'''$repeat'''の後ろに文字を入れてください")
+        except Exception as e:
+            print(e.args)
+        return
+    # オウム返し２
+    if isCommand(message,"oumu$"):
+        await message.channel.send("このコマンドはターミナルから入力をする時に使うコマンドです")
+        oumu = input("ここに文字を入力してオウム返し-->")
+        await message.channel.send(oumu)
+        return
     # お遊び要素
-    if isCommand(message,"$あなたは"):
+    if isCommand(message,"あなたは(|ロボットですか？)$"):
         await message.add_reaction("❌")
         async with message.channel.typing():
             await asyncio.sleep(5)
         msg = await message.reply("ﾆﾝｹﾞﾝﾀﾞﾖ")
         await msg.add_reaction("⭕")
-        
+
         return
     if isCommand(message,"ffmpeg$"):
         kami = "<:kami:933925948259205171>"
@@ -82,6 +151,13 @@ async def on_message(message):
         except Exception as e:
             print(e.args)
         return
+    if isCommand(message,"(shibanyan|shibanyaan|しばねこ|しばにゃん|シバニャン|芝猫|芝ねこ|芝ネコ|さぎねこ|さぎにゃん詐欺にゃん|詐欺ねこ|詐欺猫|サギねこ|サギ猫)"):
+        sore = "<:sore:933926434907521064>"
+        kusa = "<:kusa:933925678867423263>"
+        try:
+            await message.channel.send(sore+"は"+kusa)
+        except Exception as e:
+            print(e.arg)
     # SIGES BOTのping値を返します
     if isCommand(message,"ping$"):
         raw_ping = client.latency
@@ -173,7 +249,7 @@ async def on_message(message):
         await message.channel.send(embed=embedData)
         return
     #----------ここからpoll機能----------
-    if isCommand(message,"que(|stion) (yes-no|vote|help)"):
+    if isCommand(message,"que(|stion) (yes(|-no)|ok(|-no)|vote|help)"):
         # セパレータによる不自然な挙動を防止
         if re.match(".*\s{2,}",message.content):
             await message.channel.send("無効なコマンドです (セパレータが連続もしくは最後に入力されています)")
@@ -187,8 +263,17 @@ async def on_message(message):
             pollData  = pollData[3:]
         # 分岐
         try:
+            # OK-NO 疑問文
+            if pollType == "ok-no" or pollType == "ok":
+                # 質問文を表示
+                embed = discord.Embed(title=pollTitle, description="", color=discord.Colour.blue())
+                voting_msg = await message.channel.send(embed=embed)
+                # リアクション追加
+                for i in range(len(list_OKNO)):
+                    await voting_msg.add_reaction(list_OKNO[i])
+                return
             # Yes-No 疑問文
-            if pollType == "yes-no":
+            if pollType == "yes-no" or pollType == "yes":
                 # 質問文を表示
                 embed = discord.Embed(title=pollTitle, description="", color=discord.Colour.blue())
                 voting_msg = await message.channel.send(embed=embed)
@@ -241,7 +326,7 @@ async def on_message(message):
             else:
                 await message.channel.send("質問形式が異なっています (2つめの引数が正しくありません)")
                 return
-        
+
         except IndexError:
             await message.channel.send("質問の入力形式に間違いがあります (引数が足りません)")
             return
@@ -249,6 +334,7 @@ async def on_message(message):
     if isCommand(message,"help$"):
         embedData = discord.Embed(title = "使用可能コマンド一覧", description = "現在メンテナンス中", color = discord.Colour(0x2ecc71))
         embedData.add_field(name = "**__$number__**", value = "１から１０までの数字のリアクションを追加します")
+        embedData.set_thumbnail(url = "https://cdn.discordapp.com/emojis/933926187619733545.webp?size=96&quality=lossless")
         '''
         embedData.add_field(name = "**__$number__**", value = "ping値を返します", inline = False)
         embedData.add_field(name = "**__$number__**", value = "ping値を返します", inline = False)
@@ -262,141 +348,62 @@ async def on_message(message):
         '''
         await message.channel.send(embed=embedData)
         return
-    
+    # なんだっけ
+    #if hoegehoge == hage:
+    #---------------実装予定のvoice機能-----------------#
+    '''
+    # Suppress noise about console usage from errors
+    if isCommand(message, "join$"):
+        if message.author.voice is None:
+            await message.channel.send("接続先が見つかりません")
+            return
+        # ボイスチャンネルに接続する
+        await message.channel.send("joined!")
+        await message.author.voice.channel.connect()
+        return
+
+    if isCommand(message, "leave$"):
+        if message.guild.voice_client is None:
+            await message.channel.send("leaving")
+            return
+
+        # 切断する
+        await message.guild.voice_client.disconnect()
+        return
+
+    if isCommand(message, "play https://youtu\.be/"):
+        # video, source = search(query)
+        #voice = get(bot.voice_clients, guild=message.guild)
+        #await message.send(f"Now playing {info['title']}.")
+
+        #voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+        #voice.is_playing()
+
+        # youtubeから音楽をダウンロードする
+        # player = await YTDLSource.from_url(url, loop=client.loop)
+
+        # await message.guild.voice_client.play(player)
+
+        # await message.channel.send('playing{}'.format(player.title))
+        return
+
+    if isCommand(message, "stop$"):
+        if message.guild.voice_client is None:
+            return
+
+        # 再生中ではない場合は実行しない
+        if not message.guild.voice_client.is_playing():
+            await message.channel.send("再生中ではありません")
+            return
+
+        message.guild.voice_client.stop()
+
+        await message.channel.send("stopped!")
+        return
+        '''
     if isCommand(message,".*$"):
         await message.add_reaction("❓")
         await message.add_reaction("🤔")
-
-'''
----------------実装予定のvoice機能-----------------
-# Suppress noise about console usage from errors
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-
-class Music(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def join(self, message, *, channel: discord.VoiceChannel):
-        """Joins a voice channel"""
-
-        if message.voice_client is not None:
-            return await message.voice_client.move_to(channel)
-
-        await channel.connect()
-
-    @commands.command()
-    async def play(self, message, *, query):
-        """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        message.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
-
-        await message.send(f'Now playing: {query}')
-
-    @commands.command()
-    async def yt(self, message, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
-
-        async with message.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            message.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
-
-        await message.send(f'Now playing: {player.title}')
-
-    @commands.command()
-    async def stream(self, message, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
-
-        async with message.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            message.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
-
-        await message.send(f'Now playing: {player.title}')
-
-    @commands.command()
-    async def volume(self, message, volume: int):
-        """Changes the player's volume"""
-
-        if message.voice_client is None:
-            return await message.send("Not connected to a voice channel.")
-
-        message.voice_client.source.volume = volume / 100
-        await message.send(f"Changed volume to {volume}%")
-
-    @commands.command()
-    async def stop(self, message):
-        """Stops and disconnects the bot from voice"""
-
-        await message.voice_client.disconnect()
-
-    @play.before_invoke
-    @yt.before_invoke
-    @stream.before_invoke
-    async def ensure_voice(self, message):
-        if message.voice_client is None:
-            if message.author.voice:
-                await message.author.voice.channel.connect()
-            else:
-                await message.send("You are not connected to a voice channel.")
-                raise commands.CommandError("Author not connected to a voice channel.")
-        elif message.voice_client.is_playing():
-            message.voice_client.stop()
-'''
-
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
-                   description='Relatively simple music bot example')
-
-
-bot.add_cog(Music(bot))
-
-
-
-
 
 
 # Bot起動
